95bc7144d116510f415b6ee0998778fb184d98f5
Chris <Chris@10.0.1.5>
2011-08-10 01:56:50 +0100

    SCAddon class

---
+++ src/com/deranged/tvbo/SCAddon.java
@@ -0,0 +1,20 @@
+package com.deranged.tvbo;
+
+public class SCAddon extends SCStructure {
+
+	private String attachedTo;
+	
+	public SCAddon(Model model, String name) {
+		super(model, name);
+	}
+
+	public String getAttachedTo() {
+		return attachedTo;
+	}
+
+	public void setAttachedTo(String attachedTo) {
+		this.attachedTo = attachedTo;
+	}
+
+	
+}
